6093b30a6e2017d92acb5e7657de0b2be22edaf9
郑标 <qipbbn@gmail.com>
2019-09-01 19:16:17 +0800

    fix auth

--- demo/list.demo.js
+++ demo/list.demo.js
@@ -1,14 +1,12 @@
 var d = require('../index')();
 
 d.list({
-  userId: 'hass-oauth2',
-  userToken:
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIyMmRmNWE4NjAyMTU0YjI2OTA0OGY5NzViODEzZmE5ZiIsImlhdCI6MTU2NzMyNTY1MywiZXhwIjoxNTY3MzI3NDUzfQ.dURoYbHHLJb6ZCMDkLr6ifSrzx6-UcVKIU4hDg6J1S8',
-  refreshToken:
-    'c9ab3eda1f233a7be4de1791bad7f91bab966893e55089109cc797daab7fae10d9493b0c8f3aba56c7d61e22b2b701abf96d3437c65a0b1488337aee4a5ed754',
-  ext1: 'http://10.0.0.180:8123',
-  ext2: 'http://festive-monarch.glitch.me/',
-  ext3: 'oauth2'
+  "userId": "hass-oauth2",
+  "userToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmZmYyMGU3YjYxN2U0M2RkOTg1NmViMzZlZDZkMzFiZCIsImlhdCI6MTU2NzMyNzYxNywiZXhwIjoxNTY3MzI5NDE3fQ.hmJrhs8rAu3la8D4gByYeBMkF-LkzkLmBcQojBmsMeQ",
+  "refreshToken": "cb301642f1f50a85da5790df44970655f8cde7a0dfc1936713cb74d05ccc3aecd8da2e97c3ace07c3d93e3d572043831c4938bd0656880469fc7639fcda57c98",
+  "ext1": "http://10.0.0.180:8123",
+  "ext2": "http://festive-monarch.glitch.me/",
+  "ext3": "oauth2"
 })
 .then(result => {
   console.log(JSON.stringify(result))
